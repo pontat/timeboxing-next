be24f69600baede1f341d8ecb81732e5e354af98
pontat <ponta.t.dev@gmail.com>
2024-11-03 23:27:50 +0900

    feat: hono appType

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -5,11 +5,15 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
+const route = app
+  .get('/hello', (c) => {
+    return c.json({ message: 'Hello Next.js!' })
   })
-})
+  .get('/timeboxes', (c) => {
+    return c.json({ message: 'GET request received' })
+  })
+
+export type AppType = typeof route
 
 export const GET = handle(app)
 export const POST = handle(app)
